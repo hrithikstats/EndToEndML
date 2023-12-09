68b64075f1261cd27447f2425410340ceb28b333
hrithikstats <hrithik.sharma.stats@gmail.com>
2023-12-09 14:43:28 +0530

    Logging and Exception

---
+++ app.py
@@ -0,0 +1,12 @@
+from src.EndToEndML.logger import logging
+from src.EndToEndML.exception import CustomException
+import sys
+
+if __name__=='__main__':
+    logging.info("The execution has started")
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e, sys)
